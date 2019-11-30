a4ec7809de29aad0f558f8e631746522b7469b2c
khmerdao <58039862+khmerdao@users.noreply.github.com>
2019-11-30 18:33:14 +0100

    Creation de la boucle du feu sans bouton
    le feu fonctionne en boucle

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ function rmvfeux_red()
     `;
 }
 
-function feux_orange()
-{
-    document.querySelector("#orange").innerHTML =
-    `
-    <img src="./feuorange.jpg"></div>
-    `;
-    setTimeout(rmvfeux_orange, 2000);
-}
-
 function rmvfeux_orange()
 {
     document.querySelector("#orange").innerHTML =
@@ -23,16 +14,55 @@ function rmvfeux_orange()
     `;
 }
 
+function rmvfeux_vert()
+{
+    document.querySelector("#vert").innerHTML =
+    `
+    `;
+}
+
+
+
 function feux_red()
 {
-    feux_orange();
-    document.querySelector("#rouge").className = "pipi";
     document.querySelector("#rouge").innerHTML =
     `
     <img src="./feurouge.jpg"></div>
     `;
-    setTimeout(rmvfeux_red, 5000);
+    setTimeout(rmvfeux_red, 8000); // voyant pendant 2sec
 }
 
-let button  = document.querySelector("#bouton1");
-setTimeout((button.addEventListener("click", feux_red)), 3000); 
+function feux_orange()
+{
+    document.querySelector("#orange").innerHTML =
+    `
+    <img src="./feuorange.jpg"></div>
+    `;
+    setTimeout(rmvfeux_orange, 4000); // orange voyant pendant 2s
+}
+
+function feux_vert()
+{
+    document.querySelector("#vert").innerHTML =
+    `
+    <img src="./feuvert.jpg"></div>
+    `;
+    setTimeout(rmvfeux_vert, 2000); // vert voyant pendant 2sec
+}
+
+function feuxnormale()
+{
+    feux_vert();
+    feux_orange();
+    feux_red();
+    setTimeout(feuxnormale, 10000); // rajoute 2sec de visibilité au feu rouge
+}
+
+function feuxpieton()
+{
+    feux_orange();
+    feux_red();
+}
+
+let button  = document.querySelector("#bouton1").addEventListener("click", feuxpieton);
+feuxnormale();
